c4a377c68f8ea3ca7c72f4d3c9f4adca928f1a0f
Tyron Fajardo <tyronjf44@gmail.com>
2025-02-19 17:40:44 -0500

    ChAnGeS 4

--- MyGame/MyRealGame/Assets/_MyAssets/_Scripts/TriggerScript.cs
+++ MyGame/MyRealGame/Assets/_MyAssets/_Scripts/TriggerScript.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class TriggerScript : MonoBehaviour
 {
     
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("Something entered me!");
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        Debug.Log("Stay a while!");
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        Debug.Log("Something exited me!");
-    }
+   // private void OnTriggerEnter2D(Collider2D collision)
+   // {
+   //     Debug.Log("Something entered me!");
+   // }
+   //
+   // private void OnTriggerStay2D(Collider2D collision)
+   // {
+   //     Debug.Log("Stay a while!");
+   // }
+   //
+   // private void OnTriggerExit2D(Collider2D collision)
+   // {
+   //     Debug.Log("Something exited me!");
+   // }
 }
